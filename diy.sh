de8acb26635b62cae71946fcc0f9cb13d858e16f
ywgf88 <43352863+ywgf88@users.noreply.github.com>
2020-07-18 21:16:21 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -27,6 +27,11 @@ cp -f ../banner package/base-files/files/etc/
 echo '下载ServerChan'
 git clone https://github.com/tty228/luci-app-serverchan ../diy/luci-app-serverchan
 
+echo '下载新的主题lignt and night'
+git clone https://github.com/Aslin-Ameng/luci-theme-Light.git ../diy/luci-theme-Light
+git clone https://github.com/Aslin-Ameng/luci-theme-Night.git ../diy/luci-theme-Night
+git clone https://github.com/Leo-Jo-My/luci-theme-opentomcat.git ../diy/luci-theme-opentomcat
+
 echo '下载AdGuard Home'
 git clone https://github.com/rufengsuixing/luci-app-adguardhome ../diy/luci-app-adguardhome
 
